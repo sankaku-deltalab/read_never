56a319e4dd16fae18cd6f627066bd4a5b2d12096
Sankaku <sankaku_dlt.45631@outlook.jp>
2023-07-30 16:08:13 +0000

    Delete unused test

--- test/read_never_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ReadNeverWeb.PageControllerTest do
-  use ReadNeverWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
